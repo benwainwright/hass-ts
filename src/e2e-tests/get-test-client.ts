2754a399aa30f53711eb212fc64d6474b0ddfecc
Ben Wainwright <bwainwright28@gmail.com>
2024-09-12 18:44:18 +0100

    build: setup package intercept

--- src/e2e-tests/get-test-client.ts
+++ src/e2e-tests/get-test-client.ts
@@ -1,6 +1,6 @@
 import { HassClientConfig } from "@types";
 import { TEST_HASS_HOST, TEST_HASS_PORT, TEST_HASS_TOKEN } from "@test-support";
-import { initialiseClient } from "@core";
+import { initialiseClient } from "@test-support/package-intercept";
 
 export const getTestClient = async () => {
   const config: HassClientConfig = {
