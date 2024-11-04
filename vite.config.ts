8b2b3343268410f74bbd832bdf286ec43465351a
Ben Wainwright <bwainwright28@gmail.com>
2024-11-04 15:53:07 +0000

    chore: update vite dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,23 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    setupFiles: ["./src/test-support/setup-tests.ts"],
+    globals: true,
+    include: ["src/lib/**/*.spec.ts"],
+    coverage: {
+      all: true,
+      include: ["src/lib/**/*.ts"],
+      provider: "istanbul",
+      cleanOnRerun: true,
+      reporter: ["text", "html", "lcov"],
+      thresholds: {
+        autoUpdate: true,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+        branches: 100,
+      },
+    },
+  },
 });
